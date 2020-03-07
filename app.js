7ad52a358910983a8d0572c7682959c0b2b22e75
Thao Le <ttle09@gmail.com>
2020-03-06 22:23:21 -0500

    add reset button to reset game

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-// const blocks = document.querySelectorAll('.block');
 const player1 = 'X';
 const player2 = 'O';
-let currentTurn = player1;
-const player1Moves = [];
-const player2Moves = [];
 const winningMoves = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'], ['1', '5', '9'], ['3', '5', '7']];
+const winnerHeading = document.querySelector('#winner');
+const blocks = document.querySelectorAll('.block');
+let currentTurn = player1;
+let player1Moves = [];
+let player2Moves = [];
+
+const reset = () => {
+  player2Moves = [];
+  player1Moves = [];
+  currentTurn = player1;
+  for (let i = 0; i < blocks.length; i++) {
+    blocks[i].innerHTML = '';
+  }
+  winnerHeading.innerHTML = '';
+}
 
 const handleClick = element => {
   element.innerHTML = currentTurn;
   currentTurn === player1 ? player1Moves.push(element.id) : player2Moves.push(element.id);
-  currentTurn === player1 ? currentTurn = player2 : currentTurn = player1;
   for (const val of winningMoves) {
     if (containsAll(val, player1Moves)) {
-      return alert('Player X is the WINNER!!!');
+      winnerHeading.innerHTML = 'Player X is the WINNER!!!';
     }
     if (containsAll(val, player2Moves)) {
-      return alert('Player O is the WINNER!!!');
+      winnerHeading.innerHTML = 'Player O is the WINNER!!!';
     }
   }
+  currentTurn === player1 ? currentTurn = player2 : currentTurn = player1;
 }
 
 const containsAll = (targets, playerMoves) => {
